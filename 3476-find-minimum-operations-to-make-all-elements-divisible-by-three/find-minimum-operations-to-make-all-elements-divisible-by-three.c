7fae53874fb7d80ab1309022e1d8a82ea0272ed1
Aditya Kumar <147469835+22adityakumar@users.noreply.github.com>
2024-08-10 10:10:15 +0530

    Time: 4 ms (34.84%) | Memory: 8.5 MB (28.46%) - LeetSync

---
+++ 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.c
@@ -0,0 +1,14 @@
+int minimumOperations(int* nums, int numsSize) {
+    int count=0;
+    for(int i=0;i<numsSize;i++){
+        if(nums[i]%3!=0){
+            /*if(nums[i]-1==0 ) 
+            count++;*/
+            if( (nums[i]-1 )% 3==0 )
+            count++;
+            if( (nums[i]+1 )%3==0)
+            count++;
+        }
+    }
+    return count;
+}
